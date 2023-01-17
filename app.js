3d8b9fb4766cc39e49d753bced23d7dbcec88be6
Abhishek Choudhary <abhishekch115@gmail.com>
2023-01-17 14:21:01 +0530

    created minion app, included javascript ,html and css

---
+++ app.js
@@ -0,0 +1,22 @@
+// I am not removing some comments because for my reference
+var translateBtn = document.querySelector("#btn-translate");
+var userTextElement = document.querySelector("#txt-user");
+var txtOutput = document.querySelector("#txt-translated-output");
+
+var url = "https://api.funtranslations.com/translate/minion.json";
+
+function urlGenerator(text){
+    return url+"?text="+text;
+}
+
+function onClick(){
+    var userInput = userTextElement.value;
+    fetch(urlGenerator(userInput)).then(response=> response.json()).then(json=>txtOutput.innerText=json.contents.translated);
+    
+    //console.log("clicked");
+    // //console.log(userInput);
+    // txtOutput.innerText = userInput;
+
+}
+//console.log(translateBtn);
+translateBtn.addEventListener("click",onClick);  // Notice here we don't need to pass function with Paranthesis
