fc01870c7b2d3e9d4e2c6dae2dc3ac4871260889
Abhishek Choudhary <abhishekch115@gmail.com>
2023-01-21 07:18:48 +0530

    added error handler

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ var txtOutput = document.querySelector("#txt-translated-output");
 
 var url = "https://api.funtranslations.com/translate/minion.json";
 
+function errorHandler(error){
+    console.log("There is an error "+error);
+    alert("something went wrong with server, Please try again");
+}
+
 function urlGenerator(text){
     return url+"?text="+text;
 }
 
 function onClick(){
     var userInput = userTextElement.value;
-    fetch(urlGenerator(userInput)).then(response=> response.json()).then(json=>txtOutput.innerText=json.contents.translated);
+    fetch(urlGenerator(userInput)).then(response=> response.json()).then(json=>txtOutput.innerText=json.contents.translated).catch(errorHandler)
     
     //console.log("clicked");
     // //console.log(userInput);
     // txtOutput.innerText = userInput;
 
-}
+};
 //console.log(translateBtn);
 translateBtn.addEventListener("click",onClick);  // Notice here we don't need to pass function with Paranthesis
